6f23cce674d3e27e5c871f03850d61a66f9d6571
pratik mane <pratik.mane1299@gmail.com>
2021-08-19 14:17:32 +0530

    set window titles and refactor

--- main.js
+++ main.js
@@ -23,15 +23,12 @@ function createWindow () {
     }
   })
 
-  // and load the index.html of the app.
   mainWindow.loadFile('./windows/index.html')
 
-  // Open the DevTools.
   if (!isProd) mainWindow.webContents.openDevTools()
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.webContents.send('posts', posts)
-    // console.log(posts)
   })
 
   ipcMain.on('open-post-form-modal', (event) => {
@@ -43,10 +40,13 @@ function createWindow () {
         webPreferences: {
           nodeIntegration: true,
           contextIsolation: false,
-        }
+        },
+        title: 'Create a new post',
       })
 
       modalWindow.loadFile('./windows/post-modal.html')
+
+      modalWindow.on('ready-to-show', () => modalWindow.show())
 
       modalWindow.on('close', () => {
         modalWindow = null
@@ -66,25 +66,15 @@ function createWindow () {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
